dc6cb4fe1d972683a5e5b0bbe1f78fd9d60c80f3
chance <almalhawi.nasser@gmail.com>
2017-08-19 22:05:02 +0300

    Selling not working App don't add photos

--- app/src/main/java/com/example/chance/inventoryapp/ItemsCursorAdapter.java
+++ app/src/main/java/com/example/chance/inventoryapp/ItemsCursorAdapter.java
@@ -1,14 +1,16 @@
 package com.example.chance.inventoryapp;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
+import android.net.Uri;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CursorAdapter;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.chance.inventoryapp.Data.InventoryContract.InventoryEntry;
 
@@ -18,6 +20,8 @@ import com.example.chance.inventoryapp.Data.InventoryContract.InventoryEntry;
 
 public class ItemsCursorAdapter extends CursorAdapter {
 
+
+    int itemQuantity;
 
     public ItemsCursorAdapter(Context context, Cursor c) {
         super(context, c, false);
@@ -29,34 +33,48 @@ public class ItemsCursorAdapter extends CursorAdapter {
     }
 
     @Override
-    public void bindView(View view, final Context context, Cursor cursor) {
+    public void bindView(View view, final Context context, final Cursor cursor) {
 
-        TextView nameTextView = (TextView) view.findViewById(R.id.product_name_text_view);
+        final TextView nameTextView = (TextView) view.findViewById(R.id.product_name_text_view);
         TextView priceTextView = (TextView) view.findViewById(R.id.product_price_value_text_view);
-        TextView quantityTextView = (TextView) view.findViewById(R.id.product_quantity_value_text_view);
+        final TextView quantityTextView = (TextView) view.findViewById(R.id.product_quantity_value_text_view);
 
         int nameColumnIndex = cursor.getColumnIndex(InventoryEntry.COLUMN_ITEM_NAME);
         int priceColumnIndex = cursor.getColumnIndex(InventoryEntry.COLUMN_ITEM_PRICE);
         int quantityColumnIndex = cursor.getColumnIndex(InventoryEntry.COLUMN_ITEM_QUANTITY);
 
-        Button btn = (Button) view.findViewById(R.id.sell_button);
-
-
-        String itemName = cursor.getString(nameColumnIndex);
+        final String itemName = cursor.getString(nameColumnIndex);
         double itemPrice = cursor.getDouble(priceColumnIndex);
-        int itemQuantity = cursor.getInt(quantityColumnIndex);
+        itemQuantity = cursor.getInt(quantityColumnIndex);
 
         nameTextView.setText(itemName);
         priceTextView.setText(String.valueOf(itemPrice)+"$");
         quantityTextView.setText(String.valueOf(itemQuantity));
-
+        Button btn = (Button) view.findViewById(R.id.sell_button);
+        Object object = cursor.getString(cursor.getColumnIndex(InventoryEntry._ID));
+        btn.setTag(object);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(context, "Sold", Toast.LENGTH_SHORT).show();
+                int currentId = cursor.getInt(cursor.getColumnIndex(InventoryEntry._ID));
+                Uri currentUri = Uri.withAppendedPath(InventoryEntry.CONTENT_URI, String.valueOf(currentId)); // Current URI
+
+                int rowQuan = cursor.getInt(cursor.getColumnIndex(InventoryEntry.COLUMN_ITEM_QUANTITY));
+
+                Log.e("ID", "Current ID: " + currentId + " with quaintiy: " + rowQuan);
+                Log.e("CURRENT URI ", currentUri.toString());
+                Log.e("CURRENT QUANTITY FOR  ", itemName + " is: " + rowQuan);
+
+                rowQuan--;
+
+                ContentValues cv = new ContentValues();
+                cv.put(InventoryEntry.COLUMN_ITEM_QUANTITY, rowQuan);
+
+                context.getContentResolver().update(currentUri, cv,
+                        InventoryEntry.COLUMN_ITEM_QUANTITY,
+                        null);
             }
         });
-
     }
 
 
