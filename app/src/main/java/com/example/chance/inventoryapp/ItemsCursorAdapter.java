51dca23335cbbd1d9055c17561d7c534ab6f877c
chance <almalhawi.nasser@gmail.com>
2017-08-21 17:28:50 +0300

    Selling not working App don't add photos Added Delete confirmation message

--- app/src/main/java/com/example/chance/inventoryapp/ItemsCursorAdapter.java
+++ app/src/main/java/com/example/chance/inventoryapp/ItemsCursorAdapter.java
@@ -11,6 +11,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CursorAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.chance.inventoryapp.Data.InventoryContract.InventoryEntry;
 
@@ -22,8 +23,10 @@ public class ItemsCursorAdapter extends CursorAdapter {
 
 
     // chmod 777 /data /data/data /data/data/com.application.package /data/data/com.application.package/*su
-    int itemQuantity;
-    int quantityColumnIndex;
+
+    int currentId;
+    Uri currentUriId; // Current URI
+    Uri currentUri; // Current URI
 
     public ItemsCursorAdapter(Context context, Cursor c) {
         super(context, c, false);
@@ -39,57 +42,51 @@ public class ItemsCursorAdapter extends CursorAdapter {
 
         TextView nameTextView = (TextView) view.findViewById(R.id.product_name_text_view);
         TextView priceTextView = (TextView) view.findViewById(R.id.product_price_value_text_view);
-        final TextView quantityTextView = (TextView) view.findViewById(R.id.product_quantity_value_text_view);
+        TextView quantityTextView = (TextView) view.findViewById(R.id.product_quantity_value_text_view);
+
 
         int nameColumnIndex = cursor.getColumnIndex(InventoryEntry.COLUMN_ITEM_NAME);
         int priceColumnIndex = cursor.getColumnIndex(InventoryEntry.COLUMN_ITEM_PRICE);
-        quantityColumnIndex = cursor.getColumnIndex(InventoryEntry.COLUMN_ITEM_QUANTITY);
+        int quantityColumnIndex = cursor.getColumnIndex(InventoryEntry.COLUMN_ITEM_QUANTITY);
+
 
         final String itemName = cursor.getString(nameColumnIndex);
         double itemPrice = cursor.getDouble(priceColumnIndex);
-        itemQuantity = cursor.getInt(quantityColumnIndex);
+        int itemQuantity = cursor.getInt(quantityColumnIndex);
 
         Button btn = (Button) view.findViewById(R.id.sell_button);
-        Object object = cursor.getString(cursor.getColumnIndex(InventoryEntry._ID));
-        btn.setTag(object);
-        final int currentId = cursor.getInt(cursor.getColumnIndex(InventoryEntry._ID));
-        final Uri currentUriId = Uri.withAppendedPath(InventoryEntry.CONTENT_URI, String.valueOf(currentId)); // Current URI
-        final Uri currentUri = Uri.withAppendedPath(InventoryEntry.CONTENT_URI, String.valueOf(currentId)); // Current URI
+        currentId = cursor.getInt(cursor.getColumnIndex(InventoryEntry._ID));
+        currentUriId = Uri.withAppendedPath(InventoryEntry.CONTENT_URI, String.valueOf(currentId)); // Current URI
+        currentUri = Uri.withAppendedPath(InventoryEntry.CONTENT_URI, String.valueOf(currentId)); // Current URI
         nameTextView.setText(itemName);
         priceTextView.setText(String.valueOf(itemPrice) + "$");
         quantityTextView.setText(String.valueOf(itemQuantity));
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 int rowQuan = cursor.getInt(cursor.getColumnIndex(InventoryEntry.COLUMN_ITEM_QUANTITY));
-
                 Log.e("ID", "Current ID: " + currentId + " with quaintiy: " + rowQuan);
                 Log.e("CURRENT URI ", currentUri.toString());
                 Log.e("CURRENT QUANTITY FOR  ", itemName + " is: " + rowQuan);
 
-                rowQuan--;
+                if (rowQuan > 0) {
+                    rowQuan--;
 
-                ContentValues cv = new ContentValues();
-                cv.put(InventoryEntry.COLUMN_ITEM_QUANTITY, rowQuan);
-                String where = "_id = " + currentId;
-                context.getContentResolver().update(currentUri, cv, where, null);
-                Log.e("NEW QUANTITY FOR  ", itemName + " is: " + rowQuan);
-                //quantityColumnIndex = cursor.getColumnIndex(InventoryEntry.COLUMN_ITEM_QUANTITY);
-//                int quantity = cursor.getInt(quantityColumnIndex);
-//                if (quantity > 0) {
-//                    ContentValues cv = new ContentValues();
-//                    cv.put(InventoryEntry.COLUMN_ITEM_QUANTITY, quantity - 1);
-//                    String selectionArgs[] = {String.valueOf(currentId)};
-//                    context.getContentResolver().update(
-//                            currentUri,
-//                            cv,
-//                            null,
-//                            null);
-//                    Log.e("TAG", currentUri.toString());
-//                    Log.e("TAG", "Qua" + quantity);
-//                    Log.e("TAG", "ID" + ContentUris.parseId(currentUri));
-//                    notifyDataSetChanged();
+                    ContentValues cv = new ContentValues();
+                    cv.put(InventoryEntry.COLUMN_ITEM_QUANTITY, rowQuan);
+                    String where = "_id = " + currentId;
+                    context.getContentResolver().update(currentUri, cv, where, null);
+                    Log.e("NEW QUANTITY FOR  ", itemName + " is: " + rowQuan);
+                } else Toast.makeText(context, "Can't go below 0 items", Toast.LENGTH_SHORT).show();
+//                int rowQuan = cursor.getInt(cursor.getColumnIndex(InventoryEntry.COLUMN_ITEM_QUANTITY));
+//                Log.e("ID", "Current ID: " + currentId + " with quaintiy: " + rowQuan);
+//                Log.e("CURRENT URI ", currentUri.toString());
+//                //Log.e("CURRENT QUANTITY FOR  ", itemName + " is: " + rowQuan);
+//                rowQuan--;
+//                ContentValues cv = new ContentValues();
+//                cv.put(InventoryEntry.COLUMN_ITEM_QUANTITY, rowQuan);
+//                context.getContentResolver().update(currentUriId, cv, String.valueOf(ContentUris.parseId(currentUri)), null);
+//                Log.e("NEW QUANTITY FOR  ", itemName + " is: " + rowQuan);
             }
         });
     }
